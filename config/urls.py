acf2d9cbe63067f83df5d79f2992f4cdf23978d1
watnalldt <davidaturner56@yahoo.com>
2022-08-21 12:35:07 +0100

    Configure frontend

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", TemplateView.as_view(template_name="frontend/index.html"), name="home"),
 ]
 # Enable the debug toolbar only in DEBUG mode.
 if settings.DEBUG and settings.DEBUG_TOOLBAR:
