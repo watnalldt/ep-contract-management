10c036e31b1e0b13f065cea22c16b7ef2be747c4
watnalldt <davidaturner56@yahoo.com>
2022-08-21 12:09:03 +0100

    Cofigure urls for debug-toolbar

--- config/urls.py
+++ config/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
+# Enable the debug toolbar only in DEBUG mode.
+if settings.DEBUG and settings.DEBUG_TOOLBAR:
+    urlpatterns = [path("__debug__/", (include("debug_toolbar.urls")))] + urlpatterns
