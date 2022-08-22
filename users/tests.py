9fa4335f42cb2f3f6865085281edad105c69f6db
watnalldt <davidaturner56@yahoo.com>
2022-08-22 17:06:50 +0100

    Add account manager

--- users/tests.py
+++ users/tests.py
@@ -39,12 +39,17 @@ class UserAccountTests(TestCase):
 
     def test_new_user(self):
         db = get_user_model()
-        user = db.objects.create_user("testuser@user.com", "Alice Smith", "password")
+        user = db.objects.create_user(
+            "testuser@user.com",
+            "Alice Smith",
+            "password",
+        )
         self.assertEqual(user.email, "testuser@user.com")
         self.assertEqual(user.name, "Alice Smith")
         self.assertFalse(user.is_superuser)
         self.assertFalse(user.is_staff)
         self.assertTrue(user.is_active)
+        self.assertFalse(user.is_account_manager)
 
         with self.assertRaises(ValueError):
             db.objects.create_user(email="", name="a", password="password")
